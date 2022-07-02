7ff4e6badeb8bdfa515d6aa507bd5e5f66b3ba41
Mateo <mateomautone98@gmail.com>
2022-07-01 21:33:00 -0300

    Added .gitignore

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import prison_python
 
 def main():
-    #print("Running 1 Iteration with random number choosing")
-    #print("Success" if prison_python.main_routine(algorithm='random',verbose=True) else "Fail")
-    #print("Running 1 Iteration with loop following algorithm")
+    print("Running 1 Iteration with random number choosing")
+    print("Success" if prison_python.main_routine(algorithm='random',verbose=True) else "Fail")
+    print("Running 1 Iteration with loop following algorithm")
     print("Success" if prison_python.main_routine(algorithm='tryloop',verbose=True) else "Fail")
+    # saliobien = 0
+    # saliomal = 0
+    # for intento in range(100000):
+    #     if prison_python.main_routine(algorithm='tryloop',verbose=False):
+    #         saliobien += 1
+    #     else:
+    #         saliomal += 1
     print("Finished Runs")
+    # print("Salieron Bien:", saliobien)
+    # print("Salieron Mal:", saliomal)
 
 if __name__ == '__main__':
     main()
